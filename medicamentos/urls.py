d973f3ccccdb9eec9fe7d5494f13b036d5c6b69b
hdp115grupo10 <hdp115grupo10@gmail.com>
2016-06-14 15:39:27 -0600

    changes

--- medicamentos/urls.py
+++ medicamentos/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+""lol""
 urlpatterns = [
-    url(r'^',include('MedicamentosApp.urls')),
     url(r'^admin/', admin.site.urls),
 ]
